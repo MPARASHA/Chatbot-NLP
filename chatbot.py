bfcf13cbf63c5a014d2e3fee179aeb232220c1ef
Neil <mparasha@ualberta.ca>
2021-08-30 16:51:39 -0600

    Added name recognition functionality User name is now a session variable

--- chatbot.py
+++ chatbot.py
@@ -5,6 +5,23 @@ import torch
 
 from NN import NN
 from preprocess import bagOfWords, tokenize
+import nltk
+
+# Session Variables
+name = ""
+
+def updateName(response):
+    if(name != "" and name is not None):
+        return response.replace("<HUMAN>", name)
+    else:
+        return response
+
+# Extracts the name from the input sentence using ne_chunk function of nltk library
+def extract_entities(text):
+    for sent in nltk.sent_tokenize(text):
+         for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))):
+            if (hasattr(chunk, 'label') and chunk[0][1] == 'NNP'):
+                return (' '.join(c[0] for c in chunk.leaves()))
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -28,12 +45,11 @@ model.eval()
 bot_name = "YoBot"
 print("Let's chat! (type 'quit' to exit)")
 while True:
-    # sentence = "do you use credit cards?"
-    sentence = input("You: ")
-    if sentence == "quit":
+    sentenceI = input("You: ")
+    if sentenceI == "quit":
         break
 
-    sentence = tokenize(sentence)
+    sentence = tokenize(sentenceI)
     X = bagOfWords(sentence, all_words)
     X = X.reshape(1, X.shape[0])
     X = torch.from_numpy(X).to(device)
@@ -43,11 +59,17 @@ while True:
 
     tag = tags[predicted.item()]
 
+    # if its a greeting response it will have user's name
+    if(tag == "GreetingResponse" or tag == "CourtesyGreetingResponse"):
+        name = extract_entities(sentenceI)
+
+
     probs = torch.softmax(output, dim=1)
     prob = probs[0][predicted.item()]
     if prob.item() > 0.75:
         for intent in intents['intents']:
             if tag == intent["intent"]:
-                print(f"{bot_name}: {random.choice(intent['responses'])}")
+
+                print(f"{bot_name}: {updateName(random.choice(intent['responses']))}")
     else:
         print(f"{bot_name}: I do not understand...")
